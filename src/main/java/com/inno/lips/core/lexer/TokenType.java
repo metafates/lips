4f351568c66d7149761bcef716d94fe990cd469f
metafates <vvz.dev@gmail.com>
2023-09-04 11:23:24 +0300

    refactor: allow exact patterns for tt

--- src/main/java/com/inno/lips/core/lexer/TokenType.java
+++ src/main/java/com/inno/lips/core/lexer/TokenType.java
@@ -4,28 +4,46 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.compile;
+
 public enum TokenType {
-    OPEN_PAREN(Pattern.compile("\\(")),
-    CLOSE_PAREN(Pattern.compile("\\)")),
-    SET(Pattern.compile("set")),
-    LAMBDA(Pattern.compile("lambda")),
-    COMMENT(Pattern.compile(";[^\n]*")),
-    BOOLEAN_LITERAL(Pattern.compile("false|true")),
-    NUMBER_LITERAL(Pattern.compile("[+-]?\\d+(\\.\\d*)?")),
-    BLANK(Pattern.compile("\\s")),
-    QUOTE_TICK(Pattern.compile("'")),
-    STRING_LITERAL(Pattern.compile("\"([^\"\\\\]|\\\\t|\\\\u|\\\\n|\\\\r|\\\\\")*\"")),
-    IDENTIFIER(Pattern.compile("[A-Za-z+\\-.][A-Za-z\\d\\-.]*")); // TODO
+    OPEN_PAREN("("),
+    CLOSE_PAREN(")"),
+    SET("set"),
+    LAMBDA("lambda"),
+    COMMENT(compile(";[^\n]*")),
+    BOOLEAN_LITERAL(compile("false|true")),
+    NUMBER_LITERAL(compile("[+-]?\\d+(\\.\\d*)?")),
+    BLANK(compile("\\s")),
+    QUOTE_TICK(compile("'")),
+    STRING_LITERAL(compile("\"([^\"\\\\]|\\\\t|\\\\u|\\\\n|\\\\r|\\\\\")*\"")),
+    IDENTIFIER(compile("[A-Za-z+\\-.][A-Za-z\\d\\-.]*")); // TODO
 
 
     private final Pattern pattern;
+    private final String exactPattern;
 
     TokenType(Pattern pattern) {
         this.pattern = pattern;
+        this.exactPattern = null;
+    }
+
+    TokenType(String pattern) {
+        this.pattern = null;
+        this.exactPattern = pattern;
     }
 
     public static Optional<TokenType> matchPattern(String string) {
         for (TokenType tt : TokenType.values()) {
+            if (tt.exactPattern != null) {
+                if (string.equals(tt.exactPattern)) {
+                    return Optional.of(tt);
+                }
+
+                // in this case, regex pattern will be null;
+                continue;
+            }
+
             Matcher matcher = tt.pattern.matcher(string);
 
             if (matcher.matches()) {
