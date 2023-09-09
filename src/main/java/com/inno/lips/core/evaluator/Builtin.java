c3968f831bd1bddc3c186b478e7e2dba3e0d958d
metafates <vvz.dev@gmail.com>
2023-09-09 15:39:33 +0300

    feat: add more builtins

--- src/main/java/com/inno/lips/core/evaluator/Builtin.java
+++ src/main/java/com/inno/lips/core/evaluator/Builtin.java
@@ -64,6 +64,26 @@ class Builtin {
         });
     }
 
+    private static Procedure times() {
+        return new Procedure((frame, arguments) -> {
+            if (arguments.size() < 2) {
+                throw new ArityMismatchException(frame, 2, arguments.size());
+            }
+
+            float product = 1;
+
+            for (var argument : arguments) {
+                if (!(argument.sExpression() instanceof NumberLiteral numberLiteral)) {
+                    throw new TypeException(frame, "number", argument);
+                }
+
+                product *= numberLiteral.getValue();
+            }
+
+            return new LipsObject(new NumberLiteral(new Span(), product));
+        });
+    }
+
     private static Procedure equal() {
         return new Procedure((frame, arguments) -> {
             if (arguments.isEmpty()) {
@@ -177,10 +197,141 @@ class Builtin {
                 list.addAll(sequence.getElements());
             } else if (!(right instanceof Nil)) {
                 list.add(right);
+            } else {
+                throw new TypeException(frame, "sequence or list", arguments.get(1));
             }
 
 
             return new LipsObject(new Sequence(left.span().join(right), list));
+        }));
+    }
+
+    private static Procedure eval() {
+        return new Procedure((frame, arguments) -> {
+            if (arguments.size() != 1) {
+                throw new ArityMismatchException(frame, 1, arguments.size());
+            }
+
+            var argument = arguments.get(0);
+
+            if (argument instanceof Procedure) {
+                throw new TypeException(frame, "list or atom", "procedure");
+            }
+
+            // TODO: pass valid env
+            return Evaluator.evaluate(frame, new Environment(), argument.sExpression());
+        });
+    }
+
+    private static Procedure and() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() < 2) {
+                throw new ArityMismatchException(frame, 2, arguments.size());
+            }
+
+            for (var argument : arguments) {
+                if (!argument.sExpression().asBoolean()) {
+                    return new LipsObject(new BooleanLiteral(new Span(), false));
+                }
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), true));
+        }));
+    }
+
+    private static Procedure or() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() < 2) {
+                throw new ArityMismatchException(frame, 2, arguments.size());
+            }
+
+            for (var argument : arguments) {
+                if (argument.sExpression().asBoolean()) {
+                    return new LipsObject(new BooleanLiteral(new Span(), true));
+                }
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), false));
+        }));
+    }
+
+    public Procedure isList() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() != 1) {
+                throw new ArityMismatchException(frame, 1, arguments.size());
+            }
+
+            var argument = arguments.get(0);
+
+            if (argument.sExpression() instanceof Sequence) {
+                return new LipsObject(new BooleanLiteral(new Span(), true));
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), false));
+        }));
+    }
+
+    public Procedure isNumber() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() != 1) {
+                throw new ArityMismatchException(frame, 1, arguments.size());
+            }
+
+            var argument = arguments.get(0);
+
+            if (argument.sExpression() instanceof NumberLiteral) {
+                return new LipsObject(new BooleanLiteral(new Span(), true));
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), false));
+        }));
+    }
+
+    public Procedure isBool() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() != 1) {
+                throw new ArityMismatchException(frame, 1, arguments.size());
+            }
+
+            var argument = arguments.get(0);
+
+            if (argument.sExpression() instanceof BooleanLiteral) {
+                return new LipsObject(new BooleanLiteral(new Span(), true));
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), false));
+        }));
+    }
+
+    public Procedure isNil() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() != 1) {
+                throw new ArityMismatchException(frame, 1, arguments.size());
+            }
+
+            var argument = arguments.get(0);
+
+            if (argument.sExpression() instanceof Nil) {
+                return new LipsObject(new BooleanLiteral(new Span(), true));
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), false));
+        }));
+    }
+
+    public Procedure isSymbol() {
+        return new Procedure(((frame, arguments) -> {
+            if (arguments.size() != 1) {
+                throw new ArityMismatchException(frame, 1, arguments.size());
+            }
+
+            var argument = arguments.get(0);
+
+            if (argument.sExpression() instanceof Symbol) {
+                return new LipsObject(new BooleanLiteral(new Span(), true));
+            }
+
+            return new LipsObject(new BooleanLiteral(new Span(), false));
         }));
     }
 
@@ -190,12 +341,21 @@ class Builtin {
         scope.put("println", println());
         scope.put("+", add());
         scope.put("-", sub());
+        scope.put("*", times());
         scope.put("=", equal());
         scope.put("not=", notEqual());
         scope.put("car", car());
         scope.put("cdr", cdr());
-        scope.put("not", not());
         scope.put("cons", cons());
+        scope.put("eval", eval());
+        scope.put("and", and());
+        scope.put("not", not());
+        scope.put("or", or());
+        scope.put("list?", isList());
+        scope.put("number?", isNumber());
+        scope.put("bool?", isBool());
+        scope.put("nil?", isNil());
+        scope.put("symbol?", isSymbol());
 
         return scope;
     }
